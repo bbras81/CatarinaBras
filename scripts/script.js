a449c1e7a321955e0c77b8e4611e3a10b563be1b
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2023-07-18 21:45:43 +0200

    check Buttons

--- scripts/script.js
+++ scripts/script.js
@@ -14,26 +14,39 @@ console.log(slides)
 const root = document.querySelector(':root')
 
 let slideCount = 0
+const checkButtons = document.querySelector('#checkButtons').children
+checkButtons[slideCount].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_checked</span>'
+
 
 
 function forwardArrow(){
     if (slideCount != slides.length -1){
         slideCount ++
         root.style.setProperty('--slide', '-'+ slideCount+'00%')
+        checkButtons[slideCount].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_checked</span>'
+        checkButtons[slideCount - 1].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_unchecked</span>'        
     }else{
         slideCount = 0
         root.style.setProperty('--slide', '0%')
+        checkButtons[slides.length - 1].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_unchecked</span>'
+        checkButtons[slideCount].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_checked</span>'
     }
+
+    checkButtons[slideCount].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_checked</span>'
 }
 
 function backArrow(){
     if (slideCount != 0){
         slideCount --
         root.style.setProperty('--slide', '-'+ slideCount+'00%')
+        checkButtons[slideCount].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_checked</span>'
+        checkButtons[slideCount + 1].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_unchecked</span>'    
     }else{
         slideCount = 4
         root.style.setProperty('--slide', '-400%')
+        checkButtons[slideCount].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_checked</span>'
+        checkButtons[0].innerHTML = '<span class="material-symbols-outlined buttonsHeight " onclick="">radio_button_unchecked</span>'
     }
-}
 
+}
 
