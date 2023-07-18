a5a57c204b6ca18d0d497026909ff66b5d369879
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2023-07-18 13:24:23 +0200

    Update script.js
    Retirar o Console Log

--- scripts/script.js
+++ scripts/script.js
@@ -20,24 +20,19 @@ function forwardArrow(){
     if (slideCount != slides.length -1){
         slideCount ++
         root.style.setProperty('--slide', '-'+ slideCount+'00%')
-        console.log('IF',slideCount)
     }else{
         slideCount = 0
         root.style.setProperty('--slide', '0%')
-        console.log('Else',slideCount)
     }
 }
 
 function backArrow(){
-    console.log('Fora',slideCount)
     if (slideCount != 0){
         slideCount --
         root.style.setProperty('--slide', '-'+ slideCount+'00%')
-        console.log('IF',slideCount)
     }else{
         slideCount = 4
         root.style.setProperty('--slide', '-400%')
-        console.log('Else',slideCount)
     }
 }
 
